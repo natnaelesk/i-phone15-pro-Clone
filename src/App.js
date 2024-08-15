f858e10e15156b5d733c43bfa2ce66624ad7d355
natnaelesk <natnaeleskinder41@gmail.com>
2024-08-15 17:09:23 +0300

    host

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+// import Model from './components/Model';
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
+
+// import * as Sentry from '@sentry/react';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="bg-black">
+      
+      {/* welcome! */}
+      <Navbar />
+      <Hero />
+      <Highlights />
+      {/* <Model /> */}
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
   );
 }
 
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+  // In your webpack.config.js
+module.exports = {
+    devtool: 'none', // Disables source maps
+    // Other configurations
+  };
+  
+  // eslint-disable-next-line
